ac948ee24435b3ecc0761b6c2e15eac8d06ef460
Andrew <andrew.vk90@gmail.com>
2023-05-21 21:34:42 +0300

    Token added 30 step 2

--- HW27/settings.py
+++ HW27/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework_simplejwt",
     "ads",
     "users",
 ]
@@ -124,7 +125,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ]
 }
 
 # Default primary key field type
